7900019f2f8e413e7043d4d2665090955c617626
Stiwii <stiwers1997@gmail.com>
2024-10-24 21:57:54 -0500

    SS: customize components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      Hello word
-      <Button variant={"link"}>CLick Me</Button>
-      <p className="text-4xl font-bold text-red-400">Steeven</p>
+    <div className=" ">
+      <Input />
+      <Button size={"sm"}>Primary</Button>
+      <Button variant={"secondary"}>Secondary</Button>
+      <Button variant={"destructive"}>Destructive</Button>
+      <Button variant={"ghost"}>Ghost</Button>
+      <Button variant={"muted"}>Muted</Button>
+      <Button variant={"outline"}>Outline</Button>
+      <Button variant={"teritary"}>Teritary</Button>
     </div>
   );
 }
