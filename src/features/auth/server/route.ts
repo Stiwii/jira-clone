7d161c63e0851555b1830cb93066eee851d55224
Stiwii <stiwers1997@gmail.com>
2024-10-30 23:28:42 -0500

    SS: Session Middleware

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,16 +1,40 @@
 import { Hono } from "hono";
+import { ID } from "node-appwrite";
+
 import { zValidator } from "@hono/zod-validator";
+import { createAdminClient } from "@/lib/appwrite";
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { deleteCookie, setCookie } from "hono/cookie";
 
 import { loginSchema, registerSchema } from "../schemas";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+    return c.json({ data: user });
+  })
   .post(
     "/login",
     zValidator("json", loginSchema),
 
-    (c) => {
+    async (c) => {
       const { email, password } = c.req.valid("json");
-      console.log({ email, password });
+
+      const { account } = await createAdminClient();
+
+      const session = await account.createEmailPasswordSession(email, password);
+
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "Strict",
+        maxAge: 60 * 60 * 24 * 30,
+      });
+
+      return c.json({ success: true });
+
       return c.json({ email, password });
     }
   )
@@ -18,11 +42,32 @@ const app = new Hono()
     "/register",
     zValidator("json", registerSchema),
 
-    (c) => {
+    async (c) => {
       const { name, email, password } = c.req.valid("json");
-      console.log({ name, email, password });
-      return c.json({ name, email, password });
+      const { account } = await createAdminClient();
+      await account.create(ID.unique(), email, password, name);
+
+      const session = await account.createEmailPasswordSession(email, password);
+
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "Strict",
+        maxAge: 60 * 60 * 24 * 30,
+      });
+
+      return c.json({ success: true });
     }
-  );
+  )
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
+    deleteCookie(c, AUTH_COOKIE);
+    //* await account.deleteSessions("current");
+    await account.deleteSessions();
+
+    return c.json({ success: true });
+  });
 
 export default app;
